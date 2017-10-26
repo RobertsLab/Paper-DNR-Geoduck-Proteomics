1220ed5105a3d28feed138ac8da1edb9b9b6a99f
laurahspencer <lhs3@uw.edu>
2017-10-25 20:58:43 -0700

    Update Script01-File-path-for-PECAN.R

--- analyses/DIA/Script01-File-path-for-PECAN.R
+++ analyses/DIA/Script01-File-path-for-PECAN.R
@@ -1,7 +1,7 @@
 # Execute this script to prepare a .txt file listing the .mzML data files names
 
 DIA.mzML.files <- list.files(path="../data/DIA/2017-Geoduck-DIA-raw/", pattern="*.mzML")
-write(DIA.mzML.files, file = "../data/DIA/2017-Geoduck-DIA-raw/DNR_Geoduck_mzMLpath.txt",
+write(DIA.mzML.files, file = "../analyses/DIA/2017-Geoduck-DIA-raw/DNR_Geoduck_mzMLpath.txt",
       ncolumns = 1,
       append = FALSE, sep = " ")
       
